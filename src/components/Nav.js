0367a6ca326ecf3de4017c24afdcdf1e90bbdf44
Адхам <adxamjon0806@gmail.com>
2024-02-13 16:40:45 +0500

    Initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import "../styles/Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <div className="nav_contents">
+        <img
+          className="nav_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          alt="Netflix logo"
+        />
+        <img
+          className="nav_avatar"
+          src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
+          alt="Netflix logo"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBnUi16_bzYNEuoK9Sh3BJ7lR5e5hTYi0k",
+  authDomain: "netflix-clone-8b435.firebaseapp.com",
+  projectId: "netflix-clone-8b435",
+  storageBucket: "netflix-clone-8b435.appspot.com",
+  messagingSenderId: "591379913862",
+  appId: "1:591379913862:web:dad345b2fd5ea3185f8507",
+};
